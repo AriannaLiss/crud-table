b84fe45ca483275e223bfb5f6fc42bca69a5f178
Lidiia Kalakutska <lidia.kalakutskaya@gmail.com>
2023-01-27 15:11:32 +0100

    Created form and table. React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const initialValues = {
+  userName: '',
+  userSurname: '',
+  userSalary: ''
+}
+
 function App() {
+  const [userData, setUserData] = useState(initialValues);
+  const [count, setCount] = useState(0);
+
+  console.log('userData: ', userData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="wrapper-content">
+        <div className="table-data">
+          <table>
+            <th>#</th>
+            <th>User Name</th>
+            <th>User Sername</th>
+            <th>User Salary</th>
+            <th>Actions</th>
+
+            <tbody>
+
+            </tbody>
+          </table>
+        </div>
+
+        <div>
+          <form>
+            <input placeholer="Write your name" onChange={(e) => setUserData((prevState) => ({
+              ...prevState,
+              userName: e.target.value
+            }))}/>
+            <input placeholer="Write your surname" onChange={(e) => setUserData((prevState) => ({
+              ...prevState,
+              userSurname: e.target.value
+            }))}/>
+            <input placeholer="Write your salary" onChange = {(e) => setUserData((prevState) => ({
+              ...prevState,
+              userSalary: e.target.value
+            }))}/>
+            
+            <div className="buttons-wrapper">
+              <button type="reset">Clean</button>
+              <button type="submit">Add</button>
+            </div>
+
+            <div>{count}</div>
+            <button onClick={() => {setCount(count-1)}} type="button">minus</button>
+            <button onClick={() => {setCount(count+1)}} type="button">plus</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
